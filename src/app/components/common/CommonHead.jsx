ec7df6231cd13b43d9d73348c3581bbbb7b5c4b7
codexmish <majharulislam.shanto.70@gmail.com>
2025-11-03 12:47:11 +0600

    common Head added

---
+++ src/app/components/common/CommonHead.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const CommonHead = ({ head, pp }) => {
+  return (
+    <>
+      <div className="px-6 lg:px-0">
+        <h2 className="text-[30px] lg:text-[54px] text-cofee font-bold font-playfair text-center">
+          {head}
+        </h2>
+        <p className="text-sm lg:text-xl text-secoundary-text font-normal font-playfair text-center">
+          {pp}
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default CommonHead;
