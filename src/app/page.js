128a8fe584f2d3be5f370e35277527a5e4f0d805
codexmish <majharulislam.shanto.70@gmail.com>
2025-11-03 13:48:12 +0600

    small fixes and all done

--- src/app/page.js
+++ src/app/page.js
@@ -1,25 +1,24 @@
-import React from 'react'
-import Banner from './components/Banner'
-import Discover from './components/Discover'
-import Enjoy from './components/Enjoy'
-import Different from './components/Different'
-import Chance from './components/Chance'
-import Feedback from './components/Feedback'
-import Subscribe from './components/Subscribe'
+import React from "react";
+import Banner from "./components/Banner";
+import Discover from "./components/Discover";
+import Enjoy from "./components/Enjoy";
+import Different from "./components/Different";
+import Chance from "./components/Chance";
+import Feedback from "./components/Feedback";
+import Subscribe from "./components/Subscribe";
 
 const page = () => {
   return (
     <>
-    <Banner/>
-    <Discover/>
-    <Enjoy/>
-    <Different/>
-    <Chance/>
-    <Feedback/>
-    <Subscribe/>
-    
+      <Banner />
+      <Discover />
+      <Enjoy />
+      <Different />
+      <Chance />
+      <Feedback />
+      <Subscribe />
     </>
-  )
-}
+  );
+};
 
-export default page
+export default page;
